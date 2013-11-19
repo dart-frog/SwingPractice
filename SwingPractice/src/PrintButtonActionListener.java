a7ff5a402bb8e038b66b1a3096513732fe8ec1b4
Steve <Steve@Steve-DellPC>
2013-11-18 18:30:06 -0800

    radio works

--- SwingPractice/src/PrintButtonActionListener.java
+++ SwingPractice/src/PrintButtonActionListener.java
@@ -1,6 +1,7 @@
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
+import javax.swing.ButtonGroup;
 import javax.swing.JCheckBox;
 import javax.swing.JPanel;
 import javax.swing.JRadioButton;
@@ -13,31 +14,40 @@ public class PrintButtonActionListener implements ActionListener {
 	private JCheckBox jCB2;
 	private JToggleButton jTB;
 	private JTextArea jTA;	
-	int radio;
+	private JRadioButton jRB1;
+	private JRadioButton jRB2;
+	private JRadioButton jRB3;
 	
-	public PrintButtonActionListener(JCheckBox jCB1, JCheckBox jCB2, JToggleButton jTB,JTextArea jTA,JRadioButton jRB1,JRadioButton jRB2,JRadioButton jRB3){
+	public PrintButtonActionListener(JCheckBox jCB1, JCheckBox jCB2, JToggleButton jTB,JTextArea jTA, JRadioButton jRB1, JRadioButton jRB2, JRadioButton jRB3){
 		this.jCB1 = jCB1;
 		this.jCB2 = jCB2;
 		this.jTB = jTB;
 		this.jTA = jTA;
-		if (jRB1.isSelected()){
-			radio = 1;
-		}
-		else if(jRB2.isSelected()){
-			radio =2;
-		}
-		else if(jRB3.isSelected()){
-			radio =3;
-		}
+		this.jRB1=jRB1;
+		this.jRB2=jRB2;
+		this.jRB3=jRB3;
 	}
 	
 	@Override
 	public void actionPerformed(ActionEvent arg0) {
 		 System.out.println("Check Box 1: " + Boolean.toString(jCB1.isSelected()));
 		 System.out.println("Check Box 2: " + Boolean.toString(jCB2.isSelected()));
-		 System.out.println("Radio " + radio);
+		 System.out.println("Radio " + radio());
 		 System.out.println("Toggle Button: " + Boolean.toString(jTB.isSelected()));
 		 System.out.println("Text: " + jTA.getText());
+	}
+	public int radio(){
+		int radio = 0;
+		if (jRB1.isSelected()){
+			radio = 1;
+		}
+		if (jRB2.isSelected()){
+			radio =2;
+		}
+		if (jRB3.isSelected()){
+			radio =3;
+		}
+		return radio;
 	}
 
 }
